fbec9ffc9575b5b46acfd6c835ded52c7ea29c1c
Tokitokivodka <54808191+Tokitokivodka@users.noreply.github.com>
2021-01-06 10:20:35 +0100

    correction défaut

--- CoursCarto1/src/exo2.js
+++ CoursCarto1/src/exo2.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log(position) ;
-      $('#function1').find('ul').append(
+      $('#func1').find('ul').append(
         '<li>Longitude : ' + position.coords.longitude + '</li>',
         "<li>Latitude : " + position.coords.latitude + "</li>",
         "<li>Precision : " + position.coords.accuracy + "</li>",
@@ -12,8 +12,8 @@ $(document).ready(function() {
     });
   
     navigator.geolocation.watchPosition(function(position) {
-      $('#function2').find('ul').empty() ;
-      $('#function2').find('ul').append(
+      $('#func2').find('ul').empty() ;
+      $('#func2').find('ul').append(
         '<li>Longitude : ' + position.coords.longitude + '</li>',
         "<li>Latitude : " + position.coords.latitude + "</li>",
         "<li>Precision : " + position.coords.accuracy + "</li>",
