d407f82d0b2fafa8689492061cda962de11fd3e6
Tokitokivodka <54808191+Tokitokivodka@users.noreply.github.com>
2021-01-13 11:54:51 +0100

    on croise tout

--- CoursCarto2/src/exo1.js
+++ CoursCarto2/src/exo1.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     //OpenStreetMap
     var mapOSM = L.map('mapOSM').setView([position.coords.latitude, position.coords.longitude], 10);
 
-    L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
+    L.tileLayer('https://stamen-tiles-{S}a.ssl.fastly.net/', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 15
     }).addTo(mapOSM);
