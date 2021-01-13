bde27a153ad828f966d88b815e81b85d80c1985d
Tokitokivodka <54808191+Tokitokivodka@users.noreply.github.com>
2021-01-13 11:31:00 +0100

    LA PLACE DE LA FEMME C'EST à LA CUISINE

--- CoursCarto2/src/exo1.js
+++ CoursCarto2/src/exo1.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
     //OpenStreetMap
     var mapOSM = L.map('mapOSM').setView([position.coords.latitude, position.coords.longitude], 10);
 
-    L.tileLayer('"url": ["https://stamen-tiles-{S}a.ssl.fastly.net/", layer, "/{Z}/{X}/{Y}.", type].join("")', {
+    L.tileLayer('https://stamen-tiles-{S}a.ssl.fastly.net/', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 15
     }).addTo(mapOSM);
